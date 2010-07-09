70ff15a5e2a4d7d2db1a68982748191763d5fb33
Daniel.Albert@gi-de.com <Daniel.Albert@gi-de.com@cfea21b8-c4cb-11de-81ce-f10158adbdd1>
2010-07-09 08:28:44 +0000

    removed jni folder -> merged to smartcard-api

--- trunk/jni/mcex/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE     := mcex
-LOCAL_SRC_FILES  := McexJni.cpp McexExceptions.cpp
-LOCAL_LDLIBS     := -llog -L$(SYSROOT)/usr/lib 
-include $(BUILD_SHARED_LIBRARY)
-
