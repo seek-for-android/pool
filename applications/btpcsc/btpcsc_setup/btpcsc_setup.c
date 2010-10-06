8d4ff79a5139c1f7f3b6902ec3e4e447879049f5
manueleberl <manueleberl@cfea21b8-c4cb-11de-81ce-f10158adbdd1>
2010-10-06 10:31:58 +0000

    M    btpcsc/btpcsc_config.c M    btpcsc/libbtpcsc/libbtpcsc.so M    btpcsc/btpcsc_config.h M    btpcsc/btpcsc_setup/btpcsc_setup.c M    btpcsc/btpcsc_setup/btpcsc_setup

--- applications/btpcsc/btpcsc_setup/btpcsc_setup.c
+++ applications/btpcsc/btpcsc_setup/btpcsc_setup.c
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 Manuel Eberl <manueleberl@gmx.de> for Giesecke & Devrient
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+
 /*****************************************************************
 /
 / File   :   btpcsp_setup.c
@@ -10,8 +25,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../pcsc_bluetooth.h"
-#include "../devices.h"
+#include "../btpcsc_bluetooth.h"
+#include "../btpcsc_devices.h"
 #include "../btpcsc_config.h"
 
 int arg_index, mode = -1;
