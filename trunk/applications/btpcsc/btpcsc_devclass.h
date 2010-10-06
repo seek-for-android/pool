4d321a9c1fad4e2d86a8874b447b88e61e568b56
manueleberl <manueleberl@cfea21b8-c4cb-11de-81ce-f10158adbdd1>
2010-10-06 10:34:14 +0000

    A    btpcsc/btpcsc_devclass.c A    btpcsc/btpcsc_bluetooth.c A    btpcsc/btpcsc_devclass.h A    btpcsc/btpcsc_bluetooth.h A    btpcsc/btpcsc_devices.c A    btpcsc/btpcsc_devices.h

---
+++ trunk/applications/btpcsc/btpcsc_devclass.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2010 Manuel Eberl <manueleberl@gmx.de> for Giesecke & Devrient
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+
+/*****************************************************************
+/
+/ File   :   btpcsc_devclass.h
+/ Author :   Manuel Eberl <manueleberl@gmx.de>
+/ Date   :   October 5, 2010
+/ Purpose:   Provides functions to convert device class information
+/            into human-readable text.
+/
+******************************************************************/
+
+#ifndef DEVCLASS_H
+#define DEVCLASS_H
+
+#define COD_LimitedDiscoverableMode	0x00002000
+#define COD_Networking			0x00020000
+#define COD_Rendering			0x00040000
+#define COD_Capturing			0x00080000
+#define COD_ObjectTransfer		0x00100000
+#define COD_Audio			0x00200000
+#define COD_Telephony			0x00400000
+#define COD_Information			0x00800000
+#define COD_ServiceAny			0x00FFE000
+#define COD_Invalid			0x00000000
+
+#define COD_Major_Computer		0x00000100
+#define COD_Major_Phone			0x00000200
+#define COD_Major_Lan_Access_Point	0x00000300
+#define COD_Major_Audio			0x00000400
+#define COD_Major_Peripheral		0x00000500
+#define COD_Major_Unclassified		0x00001F00
+#define COD_Major_Misc			0x00000000
+
+#define COD_Minor_Comp_Palm		0x00000014
+#define COD_Minor_Comp_Laptop		0x0000000C
+#define COD_Minor_Comp_Desktop		0x00000004
+#define COD_Minor_Comp_Server		0x00000008
+#define COD_Minor_Comp_Handheld		0x00000010
+#define COD_Minor_Comp_Unclassified	0x00000000
+
+#define COD_Minor_Phone_Smart		0x0000000C
+#define COD_Minor_Phone_Cellular	0x00000004
+#define COD_Minor_Phone_Cordless	0x00000008
+#define COD_Minor_Phone_Modem		0x00000010
+#define COD_Minor_Phone_Unclassified	0x00000000
+
+#define COD_Minor_Lan_17		0x00000020
+#define COD_Minor_Lan_33		0x00000040
+#define COD_Minor_Lan_50		0x00000060
+#define COD_Minor_Lan_67		0x00000080
+#define COD_Minor_Lan_83		0x000000A0
+#define COD_Minor_Lan_99		0x000000C0
+#define COD_Minor_Lan_NoService		0x000000E0
+#define COD_Minor_Lan_0			0x00000000
+
+#define COD_Minor_Audio_Headset		0x00000004
+#define COD_Minor_Audio_Unclassified	0x00000000
+
+#define COD_Minor_Any			0x000000FC
+
+
+void dev_service_class2str(char *result, int result_size, int dev_class);
+int dev_major2str(char *result, int result_size, int dev_class);
+void dev_minor2str(char *result, int result_size, int dev_class);
+void devclass2str(char *result, int result_size, int devclass);  
+
+#endif
