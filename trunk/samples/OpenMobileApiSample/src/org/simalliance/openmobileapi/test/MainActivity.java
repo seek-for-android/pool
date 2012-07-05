3d73ab37ba76b5e725a4f67a5c1ae070f3178722
Daniel.Albert@gi-de.com <Daniel.Albert@gi-de.com@cfea21b8-c4cb-11de-81ce-f10158adbdd1>
2012-07-05 14:05:52 +0000

    Adaptions in OpenMobileApiSample

--- trunk/samples/OpenMobileApiSample/src/org/simalliance/openmobileapi/test/MainActivity.java
+++ trunk/samples/OpenMobileApiSample/src/org/simalliance/openmobileapi/test/MainActivity.java
@@ -3,40 +3,38 @@ package org.simalliance.openmobileapi.test;
 import org.simalliance.openmobileapi.Channel;
 import org.simalliance.openmobileapi.Reader;
 import org.simalliance.openmobileapi.SEService;
-import org.simalliance.openmobileapi.Session;
 import org.simalliance.openmobileapi.SEService.CallBack;
+import org.simalliance.openmobileapi.Session;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 /**
- * This sample demonstrates how a Secure Element (SE) based android application
- * can be realized via the G&D Smart Card API .<br>
+ * Open Mobile API sample to demonstrate how a Secure Element based
+ * Android application can be realized with the G&D SmartCard API .<br>
  * 
- * Smart Card API implements the simalliance Open Mobile API ({@link http://www.simalliance.org}).
- * 
+ * SmartCard API implements the SIMalliance Open Mobile API ({@link http://www.simalliance.org}).
  */
 public class MainActivity extends Activity {
 
-	/** Open Mobile API service. */
 	SEService _service = null;
-	/** GUI elements on the screen. */
+	Session _session = null;
+
 	TextView _textview = null;
 	ScrollView _scrollview = null;
-	/** AID of the issuer security domain. */
-	private static final byte[] ISD_AID = new byte[] { (byte) 0xA0, 0x00, 0x00,
-			0x00, 0x03, 0x00, 0x00, 0x00 };
 
+	private static final byte[] ISD_AID = new byte[] { (byte) 0xA0, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00 };
+
+	
 	private static String bytesToString(byte[] bytes) {
 		StringBuffer sb = new StringBuffer();
-		for (byte b : bytes) {
+		for (byte b : bytes)
 			sb.append(String.format("%02x ", b & 0xFF));
-		}
+		
 		return sb.toString();
 	}
 
@@ -54,157 +52,103 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 
 		LinearLayout layout = new LinearLayout(this);
-		layout.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-				LayoutParams.WRAP_CONTENT));
-		layout.setOrientation(LinearLayout.VERTICAL);
-
+		layout.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 		_scrollview = new ScrollView(this);
-
 		_textview = new TextView(this);
-		_textview.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-				LayoutParams.WRAP_CONTENT));
+		_textview.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 		_scrollview.addView(_textview);
-
 		layout.addView(_scrollview);
 
 		setContentView(layout);
 
+		
 		SEServiceCallback callback = new SEServiceCallback();
 		new SEService(this, callback);
 	}
 
 	/**
-	 * Callback interface if informs that this SEService is connected to the
-	 * backend system and it's resources.
+	 * Callback interface if informs that this SEService is connected to the SmartCardService
 	 */
 	public class SEServiceCallback implements CallBack {
 
-		/**
-		 * This method will be called if this SEService is connected
-		 */
 		public void serviceConnected(SEService service) {
-			try {
-				performsOperations(service);
-			} catch (Exception e) {
-				printException(e);
-			}
+			_service = service;
+			performTest();
 		}
 	}
 
-	void performsOperations(SEService service) throws Exception {
-
-		_service = service;
-		Reader[] readers = service.getReaders();
-		logText("All available readers:  \n");
+	private void performTest() {
+		Reader[] readers = _service.getReaders();
+		logText("Available readers:  \n");
 		for (Reader reader : readers)
-			logText("	" + reader.getName() + "\n");
+			logText("	" + reader.getName() + "   - " + ((reader.isSecureElementPresent()) ? "present" : "absent") + "\n");
 
 		if (readers.length == 0) {
 			logText("No reader available \n");
-			service.shutdown();
 			return;
 		}
 
 		for (Reader reader : readers) {
-			logText("Selected Reader:" + reader.getName() + "\n");
+			if (!reader.isSecureElementPresent())
+				continue;
 
-			logText("SecureElement:");
-			boolean isPresent = reader.isSecureElementPresent();
-			logText(isPresent ? " present\n" : " absent\n");
-			logText("\n");
-			if (!isPresent)
+			logText("\n--------------------------------\nSelected reader: \"" + reader.getName() + "\"\n");
+			
+			try {
+				_session = reader.openSession();
+			} catch (Exception e) {
+				logText(e.getMessage());
+			}
+			if (_session == null)
 				continue;
 			
-			Session session = reader.openSession();
-			
-			byte[] atr = session.getATR();
-			if(atr != null)
-			{
-				logText("ATR: " + bytesToString(atr));
-				logText("\n");
+			try {
+				byte[] atr = _session.getATR();
+				logText("ATR: " + ((atr == null) ? "unavailable" : bytesToString(atr)) + "\n\n");
+			} catch (Exception e) {
+				logText("Exception on getATR(): " + e.getMessage() + "\n\n");
 			}
 
-			logText("************************\n");
-			logText("basic channel test\n");
-			logText("************************\n");
-
-			logText("\n");
-			logText("OpenBasicChannel\n");
-			Channel basicChannel = session.openBasicChannel(null);
-			logText("\n");
-
-			logText("basicChannel.transmit() \n");
-			byte[] cmd = new byte[] { (byte) 0x80, (byte) 0xCA, (byte) 0x9F,
-					0x7F, 0x00 };
-			logText(" Command: " + bytesToString(cmd) + "\n");
-			byte[] response = basicChannel.transmit(cmd);
-			logText(" Response: " + bytesToString(response) + "\n");
-			logText("\n");
-
-			logText("************************\n");
-			logText("logical channel test\n");
-			logText("************************\n");
-
-			logText("\n");
-			logText("OpenLogicalChannel\n");
-			Channel logicalChannel = session.openLogicalChannel(null);
-			logText("\n");
-
-			logText("logicalChannel.transmit() \n");
-			cmd = new byte[] { (byte) 0x80, (byte) 0xCA, (byte) 0x9F, 0x7F,
-					0x00 };
-			logText(" Command: " + bytesToString(cmd) + "\n");
-			response = logicalChannel.transmit(cmd);
-			logText(" Response: " + bytesToString(response) + "\n");
-
-			logText("\n");
-			logText("basicChannel.close()\n");
-			basicChannel.close();
-			logText("logicalChannel.close()\n");
-			logicalChannel.close();
-			logText("\n");
-
-			logText("************************\n");
-			logText("basic channel test (AID)\n");
-			logText("************************\n");
-
-			logText("\n");
-			logText("OpenBasicChannel(AID=" + bytesToString(ISD_AID) + ")\n");
-			basicChannel = session.openBasicChannel(ISD_AID);
-			logText("\n");
-
-			logText("basicChannel.transmit() \n");
-			cmd = new byte[] { (byte) 0x80, (byte) 0xCA, (byte) 0x9F, 0x7F,
-					0x00 };
-			logText(" Command: " + bytesToString(cmd) + "\n");
-			response = basicChannel.transmit(cmd);
-			logText(" Response: " + bytesToString(response) + "\n");
-
-			logText("************************\n");
-			logText("logical channel test (AID)\n");
-			logText("************************\n");
-
-			logText("\n");
-			logText("OpenLogicalChannel(AID=" + bytesToString(ISD_AID) + ")\n");
-			logicalChannel = session.openLogicalChannel(ISD_AID);
-			logText("\n");
-
-			logText("logicalChannel.transmit() \n");
-			cmd = new byte[] { (byte) 0x80, (byte) 0xCA, (byte) 0x9F, 0x7F,
-					0x00 };
-			logText(" Command: " + bytesToString(cmd) + "\n");
-			response = logicalChannel.transmit(cmd);
-			logText(" Response: " + bytesToString(response) + "\n");
-
-			logText("\n");
-			logText("basicChannel.close()\n");
-			basicChannel.close();
-			logText("logicalChannel.close()\n");
-			logicalChannel.close();
-			logText("\n");
-
+			testBasicChannel(null);
+			testBasicChannel(ISD_AID);
+			
+			testLogicalChannel(null);
+			testLogicalChannel(ISD_AID);
+			
+			_session.close();
 		}
+	}
+	
+	void testBasicChannel(byte[] aid) {
+		try {
+			logText("BasicChannel test: " + ((aid == null) ? "default applet" : bytesToString(aid)) + "\n");
+			Channel channel = _session.openBasicChannel(aid);
 
+			byte[] cmd = new byte[] { (byte) 0x80, (byte) 0xCA, (byte) 0x9F, 0x7F, 0x00 };
+			logText(" -> " + bytesToString(cmd) + "\n");
+			byte[] rsp = channel.transmit(cmd);
+			logText(" <- " + bytesToString(rsp) + "\n\n");
+
+			channel.close();
+		} catch (Exception e) {
+			logText("Exception on BasicChannel: " + e.getMessage() + "\n\n");
+		}
+	}
+		
+	void testLogicalChannel(byte[] aid) {
+		try {
+			logText("LogicalChannel test: " + ((aid == null) ? "default applet" : bytesToString(aid)) + "\n");
+			Channel channel = _session.openLogicalChannel(aid);
+
+			byte[] cmd = new byte[] { (byte) 0x80, (byte) 0xCA, (byte) 0x9F, 0x7F, 0x00 };
+			logText(" -> " + bytesToString(cmd) + "\n");
+			byte[] rsp = channel.transmit(cmd);
+			logText(" <- " + bytesToString(rsp) + "\n\n");
+
+			channel.close();
+		} catch (Exception e) {
+			logText("Exception on LogicalChannel: " + e.getMessage() + "\n\n");
+		}
 	}
 
 	@Override
@@ -214,11 +158,4 @@ public class MainActivity extends Activity {
 		}
 		super.onDestroy();
 	}
-
-	public void printException(Exception e) {
-		String msg = e.toString();
-		logText("Exception: " + msg);
-		Log.e("OpenMobileApiSample", msg);
-	}
-
 }
