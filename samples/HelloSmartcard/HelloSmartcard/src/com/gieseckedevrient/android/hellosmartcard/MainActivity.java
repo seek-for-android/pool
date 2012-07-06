d0b277ef57edf5d0074ef44997eed9f77f64fb8c
Daniel.Albert@gi-de.com <Daniel.Albert@gi-de.com@cfea21b8-c4cb-11de-81ce-f10158adbdd1>
2012-07-06 12:33:47 +0000

    Updates for 2.4.0 shared library

--- samples/HelloSmartcard/HelloSmartcard/src/com/gieseckedevrient/android/hellosmartcard/MainActivity.java
+++ samples/HelloSmartcard/HelloSmartcard/src/com/gieseckedevrient/android/hellosmartcard/MainActivity.java
@@ -11,65 +11,60 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 import org.simalliance.openmobileapi.*;
 
-/**
- * This is the refactored sample programm using only the new OMA.
- * 
- * @author user
- * 
- */
+
 public class MainActivity extends Activity implements SEService.CallBack {
 
 	final String LOG_TAG = "HelloSmartcard";
 
-	/**
-	 * API entry point
-	 */
 	private SEService seService;
 
+	private Button button;
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 
-		// do layout and UI stuff
-		LinearLayout layout = createLayout();
-		Button button = createButton();
-
-		// the OnClickListener implements the communication with the secure element
+		LinearLayout layout = new LinearLayout(this);
+		layout.setLayoutParams(new LayoutParams(
+				LayoutParams.WRAP_CONTENT, 
+				LayoutParams.WRAP_CONTENT));
+		
+		button = new Button(this);
+		button.setLayoutParams(new LayoutParams(
+				LayoutParams.WRAP_CONTENT, 
+				LayoutParams.WRAP_CONTENT));
+		
+		button.setText("Click Me");
+		button.setEnabled(false);
 		button.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				try {
-					Log.d(LOG_TAG, "Getting available readers...");
+					Log.i(LOG_TAG, "Retrieve available readers...");
 					Reader[] readers = seService.getReaders();
-
-					Log.d(LOG_TAG, "readers.length = " + readers.length);
 					if (readers.length < 1)
 						return;
 
-					Log.d(LOG_TAG, "Getting Session from the first reader...");
+					Log.i(LOG_TAG, "Create Session from the first reader...");
 					Session session = readers[0].openSession();
 
-					// Select our applet
-					Log.d(LOG_TAG,
-							"Getting logical channel from the session...");
+					Log.i(LOG_TAG, "Create logical channel within the session...");
 					Channel channel = session.openLogicalChannel(new byte[] {
 							(byte) 0xD2, 0x76, 0x00, 0x01, 0x18, 0x00, 0x02,
 							(byte) 0xFF, 0x49, 0x50, 0x25, (byte) 0x89,
 							(byte) 0xC0, 0x01, (byte) 0x9B, 0x01 });
 
-					// Send our custom hello world command
-					Log.d(LOG_TAG, "transmit()");
+					Log.i(LOG_TAG, "Send HelloWorld APDU command");
 					byte[] respApdu = channel.transmit(new byte[] {
 							(byte) 0x90, 0x10, 0x00, 0x00, 0x00 });
 
 					channel.close();
 
-					// Parse response and show String
+					// Parse response APDU and show text but remove SW1 SW2 first
 					byte[] helloStr = new byte[respApdu.length - 2];
-					System.arraycopy(respApdu, 0, helloStr, 0,
-							respApdu.length - 2);
-					Toast.makeText(MainActivity.this, new String(helloStr),
-							Toast.LENGTH_LONG).show();
+					System.arraycopy(respApdu, 0, helloStr, 0, respApdu.length - 2);
+					Toast.makeText(MainActivity.this, new String(helloStr), Toast.LENGTH_LONG).show();
 				} catch (Exception e) {
 					Log.e(LOG_TAG, "Error occured:", e);
 					return;
@@ -80,12 +75,12 @@ public class MainActivity extends Activity implements SEService.CallBack {
 		layout.addView(button);
 		setContentView(layout);
 
+		
 		try {
-			// create API entry point
-			Log.i(LOG_TAG, "creating new SEService");
+			Log.i(LOG_TAG, "creating SEService object");
 			seService = new SEService(this, this);
 		} catch (SecurityException e) {
-			Log.e(LOG_TAG, "Binding not allowed, uses-permission SMARTCARD?");
+			Log.e(LOG_TAG, "Binding not allowed, uses-permission org.simalliance.openmobileapi.SMARTCARD?");
 		} catch (Exception e) {
 			Log.e(LOG_TAG, "Exception: " + e.getMessage());
 		}
@@ -100,23 +95,7 @@ public class MainActivity extends Activity implements SEService.CallBack {
 	}
 
 	public void serviceConnected(SEService service) {
-		Log.i(LOG_TAG, "entry: seviceConnected()");
-
+		Log.i(LOG_TAG, "seviceConnected()");
+		button.setEnabled(true);
 	}
-
-	private LinearLayout createLayout() {
-		LinearLayout layout = new LinearLayout(this);
-		layout.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-				LayoutParams.WRAP_CONTENT));
-		return layout;
-	}
-
-	private Button createButton() {
-		Button button = new Button(this);
-		button.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-				LayoutParams.WRAP_CONTENT));
-		button.setText("Click Me");
-		return button;
-	}
-
 }
